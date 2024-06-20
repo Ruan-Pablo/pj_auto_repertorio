63c0e8a242a3ae8b4554352f2bf5b0cc1d287565
Ruan-Pablo <ruestacio@gmail.com>
2024-06-20 14:42:31 -0300

    ideia de usar imagem pra clicar nos botões - falha

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from time import sleep as s
 
 # abrir o youtube
 # from mouseinfo import mouseInfo
+
 # (707,160) pesquiar do youtube
 # (723,626) primeiro vídeo
 # (1258,950) 3pontin
+#   (333,941) 3ponto tela em 75%
 # (1268,836) salvar
 # (900,465) repertorio aline ou tab tab tab
-# (1202,167) X do pesquisar
+# (1202,167) apagar, X do pesquisar
 
-    
+    # TESTANDO COM A TELA EM 75% - pra ver se o botao para de sair do lugar
 
 s(1)
 def abrirYoutube():
@@ -31,7 +33,24 @@ def altTab():
     pg.keyDown('tab')
     pg.keyUp('alt')
 
+def clicar_repertorio():
+    pos75 = (844,490) #75%
+    pg.click(pos75,interval=0.5)
+
+
 def salvar_tab():
+    for i in range(4):
+        pg.press('tab', interval=1)
+        print('tab')
+        s(1)
+    s(1)
+    pg.press('space')
+    s(1)
+    pg.press('esc')
+    s(1)
+    pg.press('esc')
+
+def pontin_tab():
     # pg.click(1260,986, interval=0.5) #clicar nos 3 pontin do youtube
     # cont = 0
     for i in range(0,18):
@@ -40,15 +59,19 @@ def salvar_tab():
         # cont+=1
         print("tab")
     pg.press('space')
-    pg.press(['tab','tab'], interval=1)
+    # salvar_tab()
     s(1)
-    pg.press('space')
-    s(1)
-    pg.click(900,465,interval=0.5)# repertorio
+    clicar_repertorio() # repertorio
     s(2)
     pg.press('esc')
     s(1)
     pg.press('esc')
+
+def pontin_click():
+    pos75 = (1336,951)
+    pg.click(pos75, interval=1)
+
+
 
 
 
@@ -56,19 +79,42 @@ arq = open('repertorio.txt', 'r')
 lines = arq.readlines()
 print(lines)
 
+pos_pesquisar75 = (737,156)
+pos_segundo_vid75 = (646,599)
+pos_segundo_vid75 = (646,599)
+apagar_pesq75 = (1140,158)
 s(4)
+encontrado = True
 for music in lines: #aqui ja está com a page da yt aberta
-    s(2)
-    pg.click(54,489) #click fora
+
     pyperclip.copy(music)
-    pg.click(707,160, interval=1) #pesquisar
+    pg.click(pos_pesquisar75, duration=1) #pesquisar
     s(1)
     pg.hotkey('ctrl','v')
     pg.press('enter')
     s(5) 
-    pg.click(646,599, interval=0.5) # seleciona o primeiro
+    pg.click(pos_segundo_vid75, duration=1) # seleciona o primeiro
     # pg.press('enter')
     s(3)
-    salvar_tab()
-    pg.click(1202,167, interval=1) # apagar a pesquisa
 
+
+    # pontin_tab()
+    pontin_click()
+    while encontrado:
+        try:
+            pg.click('salvar.png', duration=1)
+            encontrado = False
+        except pg.ImageNotFoundException:
+            print("Imagem SALVAR nao encontrada.")
+    encontrado = True
+    s(1)
+    while encontrado:
+        try:
+            pg.click('repertorio.png', duration=1)
+            encontrado = False
+        except pg.ImageNotFoundException:
+            print("Imagem ALINE nao encontrada.")
+    encontrado = True
+    pg.click(apagar_pesq75, duration=1) # apagar a pesquisa
+    s(2)
+    pg.click(54,489) #click fora
--- teste.py
+++ teste.py
@@ -5,10 +5,27 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Edge()
+# driver = webdriver.Edge()
 
-driver.get("https://www.youtube.com/watch?v=a4ARV1sxOn8&t=628s")
-s(10)
-element = driver.find_element(By.XPATH, "//tp-yt-paper-dialog")
+# driver.get("https://www.youtube.com/watch?v=a4ARV1sxOn8&t=628s")
+# s(10)
+# element = driver.find_element(By.XPATH, "//tp-yt-paper-dialog")
 # /html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog
-print(element)
+
+pos = (670,155)
+pos_f = (960,155)
+def verifica_selecao():
+    pos75_inicial = (1367,837)
+    pos75_final = (1447,840)
+    pg.move()
+
+# pg.drag(pos, 0, duration=0.5)
+# pg.moveTo(pos, duration=0.5, tween=pg.easeInOutQuad)
+# pg.dragTo(pos_f, button='left')
+# cop = pg.hotkey('ctrl', 'c')
+pg.click('salvar.png', duration=2)
+exit()
+pos = (737,156)
+s(2)
+pg.click(pos, interval=1)
+# print(element)
